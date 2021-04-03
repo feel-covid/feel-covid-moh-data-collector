90212f0acf5a2a1189e6159077c814f53dadef78
Sason Braha <sasonbraha@gmail.com>
2021-04-03 10:21:40 +0300

    Use environment variable for how much days to publish

--- lib/resources/publish_resource.py
+++ lib/resources/publish_resource.py
@@ -6,49 +6,10 @@ from flask_restful import Resource
 
 from moh_request import moh_request
 
-
-def _aggregate_ird(moh_response):
-    daily_ird_dict = {
-        current_day['date']: {'infected': current_day['amount'], 'recovered': current_day['recovered'],
-                              'date': current_day['date']} for current_day in moh_response[1]['data'][-20:]}
-
-    for current_day in moh_response[5]['data'][-20:]:
-        daily_ird_dict[current_day['date']]['deceased'] = current_day['amount']
-
-    return list(daily_ird_dict.values())
-
-
-def _aggregate_test_amount(moh_response):
-    test_amount = []
-
-    for current_day in moh_response[6]['data'][-20:]:
-        test_amount.append({
-            'date': current_day['date'],
-            'amount': current_day['amountVirusDiagnosis'],
-            'positive': 0 if current_day['positiveAmount'] == -1 else round(
-                current_day['positiveAmount'] / current_day['amountVirusDiagnosis'] * 100, 2)
-        })
-
-    return test_amount
-
-
-def _aggregate_vaccinations(moh_response):
-    return [
-        {
-            'date': current_day['Day_Date'],
-            'first_dose_amount': current_day['vaccinated'],
-            'first_dose_percentage': current_day['vaccinated_population_perc'],
-            'first_dose_cumulative': current_day['vaccinated_cum'],
-            'second_dose_amount': current_day['vaccinated_seconde_dose'],
-            'second_dose_percentage': current_day['vaccinated_seconde_dose_population_perc'],
-            'second_dose_cumulative': current_day['vaccinated_seconde_dose_cum']
-        } for current_day in moh_response[7]['data'][-20:]
-    ]
-
-
 class PublishResource(Resource):
     def __init__(self):
         super().__init__()
+        self.days_amount_to_update = int(os.getenv('DAYS_AMOUNT_TO_UPDATE'))
 
     def gen_post_struct(self, data_key, data):
         return {
@@ -58,11 +19,47 @@ class PublishResource(Resource):
             }
         }
 
+    def _aggregate_ird(self, moh_response):
+        daily_ird_dict = {
+            current_day['date']: {'infected': current_day['amount'], 'recovered': current_day['recovered'],
+                                  'date': current_day['date']} for current_day in moh_response[1]['data'][-self.days_amount_to_update:]}
+
+        for current_day in moh_response[5]['data'][-self.days_amount_to_update:]:
+            daily_ird_dict[current_day['date']]['deceased'] = current_day['amount']
+
+        return list(daily_ird_dict.values())
+
+    def _aggregate_test_amount(self, moh_response):
+        test_amount = []
+
+        for current_day in moh_response[6]['data'][-self.days_amount_to_update:]:
+            test_amount.append({
+                'date': current_day['date'],
+                'amount': current_day['amountVirusDiagnosis'],
+                'positive': 0 if current_day['positiveAmount'] == -1 else round(
+                    current_day['positiveAmount'] / current_day['amountVirusDiagnosis'] * 100, 2)
+            })
+
+        return test_amount
+
+    def _aggregate_vaccinations(self, moh_response):
+        return [
+            {
+                'date': current_day['Day_Date'],
+                'first_dose_amount': current_day['vaccinated'],
+                'first_dose_percentage': current_day['vaccinated_population_perc'],
+                'first_dose_cumulative': current_day['vaccinated_cum'],
+                'second_dose_amount': current_day['vaccinated_seconde_dose'],
+                'second_dose_percentage': current_day['vaccinated_seconde_dose_population_perc'],
+                'second_dose_cumulative': current_day['vaccinated_seconde_dose_cum']
+            } for current_day in moh_response[7]['data'][-self.days_amount_to_update:]
+        ]
+
     def post(self):
         moh_response = moh_request()
-        daily_ird_data = self.gen_post_struct('dailyStatsData', _aggregate_ird(moh_response))
-        daily_test_amount_data = self.gen_post_struct('testsData', _aggregate_test_amount(moh_response))
-        daily_vaccinations_data = self.gen_post_struct('dailyVaccinationsData', _aggregate_vaccinations(moh_response))
+        daily_ird_data = self.gen_post_struct('dailyStatsData', self._aggregate_ird(moh_response))
+        daily_test_amount_data = self.gen_post_struct('testsData', self._aggregate_test_amount(moh_response))
+        daily_vaccinations_data = self.gen_post_struct('dailyVaccinationsData', self._aggregate_vaccinations(moh_response))
 
         daily_ird_url = f'{os.getenv("FEEL_API_URL")}/api/daily-ird'
         daily_test_amount_url = f'{os.getenv("FEEL_API_URL")}/api/daily-test-amount'
